f5d1c34555d40071339efe8e9dcfae8035e42782
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-01-14 00:38:01 -0500

    commiting changes

--- wailani-app/src/App.js
+++ wailani-app/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Toolbar from './Components/Toolbar/Toolbar';
+import {Route} from 'react-router';
 import './App.css';
 
 function App() {
@@ -8,6 +9,15 @@ function App() {
       <Toolbar/>
     </div>
   );
-}
+  <Fragment>
+    <Route path='/products' component={Products}/>
+    {/* The products component will have a cards component made of cards with the 
+    products */}
+    <Route path='/about' component={About}/>
+    {/* About is a component with the description of Wailani */}
+  </Fragment>
+  
+
+};
 
 export default App;
--- wailani-app/src/Components/Toolbar/Toolbar.js
+++ wailani-app/src/Components/Toolbar/Toolbar.js
@@ -4,6 +4,8 @@ import classes from './Toolbar.module.css';
 const Toolbar = () => {
     return (
         <div className={classes.Toolbar}>
+            {/* change the logo div to an image later */}
+            <div className={classes.Logo}>Logo</div>
             
         </div>
     );
