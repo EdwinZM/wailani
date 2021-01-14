e85f73baf97817ef44c65a3abf8fb11833203f33
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-01-13 19:07:36 -0500

    initializing wailani app

---
+++ wailani-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Toolbar from './Components/Toolbar/Toolbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Toolbar/>
+    </div>
+  );
+}
+
+export default App;
