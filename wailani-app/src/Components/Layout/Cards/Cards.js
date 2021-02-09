93448eb1dd3a1811237ed4783e95a9bc29f24414
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-02-08 20:32:56 -0500

    Commiting

--- wailani-app/src/Components/Layout/Cards/Cards.js
+++ wailani-app/src/Components/Layout/Cards/Cards.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import image1 from '../../../Images/unnamed.png'
 import Card from './Card/Card';
 import classes from './Cards.module.css';
--- wailani-app/src/Containers/Products.js
+++ wailani-app/src/Containers/Products.js
@@ -7,7 +7,7 @@ function Products() {
     return (
         <div className={classes.Products}>
             <Toolbar/>
-            <LinearCard title={'Title'}>Nulla sunt laboris laboris consectetur. Incididunt mollit enim dolor sit ad consectetur tempor consequat mollit. Nostrud anim est officia amet et aliqua veniam consequat Lorem id duis in. Lorem aliqua nisi voluptate ea nostrud. Ex commodo labore eu pariatur aliqua ipsum excepteur. Aliquip exercitation reprehenderit adipisicing velit exercitation Lorem nisi. Ex nisi exercitation culpa esse cillum non culpa dolore amet.</LinearCard>
+            <LinearCard title={'Title'}>Nulla sunt laboris laboris consectetur. </LinearCard>
         </div>
     )
 };
